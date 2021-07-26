fbb838c32ea5b974413ee338f1e0147e814d7852
phwms2005 <phwms2005@gmail.com>
2021-07-27 03:35:37 +0900

    update project files including gradles

--- app/src/main/java/com/example/a16sounds/InitActivity.kt
+++ app/src/main/java/com/example/a16sounds/InitActivity.kt
@@ -1,12 +1,21 @@
 package com.example.a16sounds
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 
 class InitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init)
+
+        /*
+        //Call MainActivity
+        val intent = Intent(this, MainActivity::class.java).apply{
+            //TODO
+        }
+        startActivity(intent)*/
 
     }
 }
--- app/src/main/java/com/example/a16sounds/MainActivity.kt
+++ app/src/main/java/com/example/a16sounds/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a16sounds
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
